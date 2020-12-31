aedfa2eeafa16e871dc00b67d36607631e4dce75
Vitaliy Derda <derdavitaliy@gmail.com>
2020-12-31 22:42:06 +0200

    added redux store and auth reducers/actions/operations

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //Components
 import App from 'components/App';
+//Redux
+import { Provider } from 'react-redux';
+import { store, persistor } from 'redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 //Styles
-import GlobalStyle from './index.styles';
+import GlobalStyles from './index.styles';
 
 ReactDOM.render(
-	<>
-		<App />
+	<Provider store={store}>
+		<PersistGate loading={null} persistor={persistor}>
+			<App />
 
-		<GlobalStyle />
-	</>,
+			<GlobalStyles />
+		</PersistGate>
+	</Provider>,
 	document.getElementById('root'),
 );
