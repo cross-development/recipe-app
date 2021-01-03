4c3e8c3b983e7d72338e899c39d70b18bf61d5ee
Vitaliy Derda <derdavitaliy@gmail.com>
2021-01-03 23:33:57 +0200

    fix server: app.js

--- server/server/app.js
+++ server/server/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 //Middleware
 const cors = require('cors');
+const morgan = require('morgan');
 require('dotenv').config();
 //Router
 const userRouter = require('../api/users/user.router');
